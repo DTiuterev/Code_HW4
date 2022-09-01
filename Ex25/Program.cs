dce6d199f356bbb0b52b3511c4bf7e6fc26737f7
Dmitrii <dtiuterev@rambler.ru>
2022-09-01 11:50:47 +0300

    added Ex25

---
+++ Ex25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ввод чисел А и В, возведение А в натуральную степень В
+Console.WriteLine("Введите числ А и В, я возведу число А в натуральную степень В");
+Console.WriteLine("Введите число А: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+double b = Convert.ToDouble(Console.ReadLine());
+int natural_b = Convert.ToInt32(b);
+
+double result = Math.Pow(a, Math.Abs(natural_b));
+
+Console.WriteLine($"Число {a} в натуральной степени {b} равно {result}");
